c0ffc22a5ab7cf1139d53f491106526d3db40ef1
Kenny Park <kennyparc@gmail.com>
2021-09-16 15:25:06 -0700

    Use the style property of element objects to change your car's top and left box offset styles. #3

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var $imgElement = document.querySelector('img');
-window.addEventListener('keydown', arrowKey);
-function arrowKey(event) {
+var imageStyle = $imgElement.style;
+// var intervalID = setInterval(moveCar, 16);
+window.addEventListener('keydown', keyPress);
+function keyPress(event) {
   if (event.key === 'ArrowDown') {
     $imgElement.className = 'S';
     data.carDirection = 'S';
@@ -14,11 +16,15 @@ function arrowKey(event) {
     $imgElement.className = 'E';
     data.carDirection = 'E';
   }
+  if (event.key === ' ') {
+    setInterval(moveCar, 16);
+  }
 }
 
-window.addEventListener('keydown', spaceBar);
-function spaceBar(event) {
-
+var leftValue = 5;
+function moveCar() {
+  imageStyle.left = leftValue + 'px';
+  leftValue += 5;
 }
 
 var data = {
